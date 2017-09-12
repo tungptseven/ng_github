4d3e25291f1ca3c19e56b1b6062cf03c5fa8119b
tungptseven <tungpt.seven@gmail.com>
2017-09-12 17:59:25 +0700

    list repos with http.get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+export class Repo {
+  id: number;
+  name: string;
+}
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1> {{title}} </h1>
+  <ul>
+    <li *ngFor='let repo of data'>
+      <span> {{ repo.name }} </span>
+    </li>
+  </ul>
+  `,
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  title = 'Github demo';
+  data = [];
+
+  constructor(
+    private http: Http
+  ) {}
+
+  ngOnInit() {
+    this.http.get('https://api.github.com/users/tungptseven/repos')
+    .subscribe(res => {
+      // console.log(res.json());
+      console.log(res);
+      this.data = res.json();
+      console.log(this.data);
+    });
+  }
 }
